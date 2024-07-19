409a33c4681b6522c82e782b5cc17d191d480adf
theprithwisingh <prithwisingh77@gmail.com>
2024-07-20 01:06:26 +0530

    first commit

---
+++ admin/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+     hello admin panel! 
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      frontend!
+    </>
+  )
+}
+
+export default App
