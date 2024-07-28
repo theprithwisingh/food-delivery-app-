fc4fe939dcec0accfa5f90de097460a24913626f
theprithwisingh <prithwisingh77@gmail.com>
2024-07-28 23:02:04 +0530

    update

--- frontend/src/Components/FoodItem/FoodItem.jsx
+++ frontend/src/Components/FoodItem/FoodItem.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets';
+import { StoreContext } from '../../Context/StoreContext';
 const FoodItem = ({id,name,price,description,image}) => {
     const [itemCount,setItemCount]  = useState(0);
+    const[cartItems,addToCart,removeFromCart] = useContext(StoreContext);
   return (
     <div className='food-item'>
       <div className='food-item-img-container'>
