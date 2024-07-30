61554721cda413bb76fb5cfe805190b35803eb78
theprithwisingh <prithwisingh77@gmail.com>
2024-07-30 23:28:22 +0530

    Create Frontend food delivery website

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Header from '../../Components/Header/Header';
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu';
 import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay';
+import AppDownload from '../../Components/AppDownload/AppDownload';
 const Home = () => {
   const [category, setCategory] = useState()
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
+      <AppDownload/>
     </div>
   )
 }
