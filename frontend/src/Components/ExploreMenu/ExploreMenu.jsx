092a2869da5792871372b6e0393a5dee64d4d898
theprithwisingh <prithwisingh77@gmail.com>
2024-07-31 20:32:18 +0530

    Add  some resposiveness in frontend

--- frontend/src/Components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/Components/ExploreMenu/ExploreMenu.jsx
@@ -3,14 +3,17 @@ import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 const ExploreMenu = ({category,setCategory}) => {
   return (
-    <div className='explore-menu'>
+    <div className='explore-menu' id='explore-menu'>
       <h1>Explore our menu</h1>
       <p className='explore-menu-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dolorum, distinctio, numquam id et obcaecati optio.</p>
       <div className='explore-menu-list'>
        {menu_list.map((item,index)=>{
         return (
         <div key={index} className='explore-menu-list-item'>
-           <img onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
+           <img 
+                onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} 
+                className={category===item.menu_name?"active":""} src={item.menu_image} alt="" 
+           />
            <p>{item.menu_name}</p>
         </div>)
        })}
--- frontend/src/Components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/Components/FoodDisplay/FoodDisplay.jsx
@@ -9,7 +9,7 @@ const FoodDisplay = ({category}) => {
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item,index)=>{
-          if (category==="All"||category===item.category) {
+          if (category==="All" || category===item.category) {
             return <FoodItem 
             key={index} 
             id={item._id}
