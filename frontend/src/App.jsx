78ffa66541cea10e085c6dff1ba135ad36f1dd80
theprithwisingh <prithwisingh77@gmail.com>
2024-08-01 23:27:19 +0530

    create login/signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,15 @@ import Home from './Pages/Home/Home';
 import Cart from './Pages/Cart/Cart';
 import PlaceOrder from './Pages/PlaceOrder/PlaceOrder';
 import Footer from './Components/Footer/Footer';
+import LoginPopup from './Components/LoginPopup/LoginPopup';
+import { useState } from 'react';
 function App() {
-
+    const [showLogin, setShowLogin] = useState(false);
   return (
     <>
+    {!showLogin?<LoginPopup setShowLogin={setShowLogin} />:<></>}
     <div className='App'>
-     <Navbar/>
+     <Navbar setShowLogin={setShowLogin}/>
      <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='cart' element={<Cart/>}/>
