326919595ab9af17a5e08ea044e15000dfd33c13
theprithwisingh <prithwisingh77@gmail.com>
2024-08-08 21:35:09 +0530

    foodItem Controllers

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async()=>{
+//     await mongoose.connect('mongodb+srv://prithwisingh77:prithwi@139@cluster0.apq3a2d.mongodb.net/food-delivery').then(()=>console.log("DB Connected"));
+// }
+
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://prithwisingh77:prithwi%40139@cluster0.apq3a2d.mongodb.net/food-delivery');
+    console.log("DB Connected");
+  } catch (error) {
+    console.error("DB Connection Error:", error);
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+// const express = require("express");
+// const cors = require("cors");
+// const { connect } = require("mongoose");
+import { connectDB } from "./config/db.js";
 
 //app config
 const app = express();
-const port = 4000
+const port = 4003;
 
 //middleware
 app.use(express.json())
 app.use(cors())
+
+//db connection
+connectDB();
 
 //API
 app.get("/",(req,res)=>{
@@ -18,3 +25,4 @@ app.listen(port,()=>{
     console.log(`server started on http://localhost:${port}`)
 })
 
+//mongodb+srv://prithwisingh77:prithwi@139@cluster0.apq3a2d.mongodb.net/?
