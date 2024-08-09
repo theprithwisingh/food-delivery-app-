197a0cf8b0856e01cf682ddecdeecd9f3454d901
theprithwisingh <prithwisingh77@gmail.com>
2024-08-09 22:10:36 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -3,27 +3,26 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 
-//app config
+// Initialize Express app and set the port number
 const app = express();
 const port = 4004;
 
-//middleware
+// Middleware to parse JSON and enable CORS for cross-origin requests
 app.use(express.json());
 app.use(cors());
 
-//db connection
+// Connect to the database
 connectDB();
 
-//API endPoint
-app.use("/api/food",foodRouter);
+// Set up the API route for food-related operations
+app.use("/api/food", foodRouter);
 
-//API
-app.get("/",(req,res)=>{
+// Root endpoint to verify if the API is working
+app.get("/", (req, res) => {
     res.send("API Working");
-})
+});
 
-app.listen(port,()=>{
-    console.log(`server started on http://localhost:${port}`);
-})
-
-//mongodb+srv://prithwisingh77:prithwi@139@cluster0.apq3a2d.mongodb.net/?
+// Start the server and listen on the specified port
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
