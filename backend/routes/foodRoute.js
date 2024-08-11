44f1be9eeca323ed96e3041d491e9a0df0c7120c
theprithwisingh <prithwisingh77@gmail.com>
2024-08-11 22:26:58 +0530

    food router

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -3,8 +3,16 @@ import { addFood } from "../controllers/foodController";
 import multer from "multer";
 
 const foodRouter = express.Router();
+//Image Storage Engine
+const storage  =multer.diskStorage({
+    destination:"uploads",
+    filename:(req,file,cd)=>{
+        return cd(null,`${Date.now()}${file.originalname}`)
+    }
+})
 
-
+const upload = multer({ storage: storage });
+foodRouter.post("/add",addFood)
 
 
 
