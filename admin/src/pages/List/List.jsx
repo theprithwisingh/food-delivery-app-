9dfd073f822478e2d81b899d2cd915937fe9d9f9
theprithwisingh <prithwisingh77@gmail.com>
2024-09-06 01:00:10 +0530

    update

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -15,13 +15,45 @@ const List = () => {
       toast.error("Error")
     }
   }
+
+  const removeFood= async(foodId)=>{
+    console.log(foodId);
+    const response = await axios.post(`${url}/api/food/remove`, {id:foodId});
+    await fetchList();
+    if (response.data.success) {
+      toast.success("successfully remove food item");
+    }
+    else{
+      toast.error('Error');
+    }
+  }
   useEffect(()=>{
     fetchList();
   },[])
 
   return (
-    <div>
-      
+    <div className='list add flex-col'>
+       <p>All Foods List</p>
+       <div className='list-table'>
+         <div className='list-table-format title'>
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+         </div>
+         {list.map((item,index)=>{
+            return(
+              <div key={index} className='list-table-format'>
+                <img src={`${url}/images/`+item.image} alt="" />
+                <p>{item.name}</p>
+                <p>{item.category}</p>
+                <p>{item.price}</p>
+                <p onClick={()=>removeFood(item._id)}>X</p>
+              </div>
+            )
+         })}
+       </div>
     </div>
   )
 }
